a8de311bf7422cb26cd43c41adfbe2631eceba15
praveenniroula1 <101380394+praveenniroula1@users.noreply.github.com>
2023-12-31 11:17:13 +1030

    worked on controller like filter by category price search filter single product page

--- ROutes/productRoutes.js
+++ ROutes/productRoutes.js
@@ -4,7 +4,13 @@ import {
   deleteProductController,
   getProductController,
   getSingleProductController,
+  productCategoryController,
+  productCountController,
+  productFiltersController,
+  productListController,
   productPhotoController,
+  relatedProductController,
+  searchProductController,
   updateProductController,
 } from "../controller/productController.js";
 import { requireSignIn, isAdmin } from "../middlewares/authMiddleware.js";
@@ -30,6 +36,24 @@ router.get("/product-photo/:pid", productPhotoController);
 
 // Delete product
 router.delete("/delete-product/:pid", deleteProductController);
+
+// filter product
+router.post("/product-filters", productFiltersController);
+
+// count the product
+router.get("/product-count", productCountController);
+
+// product per page
+router.get("/product-list/:page", productListController);
+
+// product search
+router.get("/search/:keyword", searchProductController);
+
+// Similar search
+router.get("/related-product/:pid/:cid", relatedProductController);
+
+// Categories wise product
+router.get("/product-category/:slug", productCategoryController);
 
 // Update product
 router.put(
--- controller/productController.js
+++ controller/productController.js
@@ -1,4 +1,5 @@
 import productModel from "../models/productModel.js";
+import categoryModel from "../models/categoryModel.js";
 import fs from "fs";
 import slugify from "slugify";
 
@@ -98,6 +99,7 @@ export const getSingleProductController = async (req, res) => {
   }
 };
 
+// get photo
 export const productPhotoController = async (req, res) => {
   try {
     const product = await productModel.findById(req.params.pid).select("photo");
@@ -109,30 +111,31 @@ export const productPhotoController = async (req, res) => {
     console.log(error);
     res.status(500).send({
       success: false,
+      message: "Erorr while getting photo",
       error,
-      message: "Error in Getting product photo",
     });
   }
 };
 
+//delete controller
 export const deleteProductController = async (req, res) => {
   try {
     await productModel.findByIdAndDelete(req.params.pid).select("-photo");
     res.status(200).send({
       success: true,
-      message: "Successfully deleted product",
+      message: "Product Deleted successfully",
     });
   } catch (error) {
     console.log(error);
     res.status(500).send({
       success: false,
+      message: "Error while deleting product",
       error,
-      message: "Error in Getting product photo",
     });
   }
 };
 
-// update
+//upate producta
 export const updateProductController = async (req, res) => {
   try {
     const { name, description, price, category, quantity, shipping } =
@@ -177,6 +180,131 @@ export const updateProductController = async (req, res) => {
       success: false,
       error,
       message: "Error in Updte product",
+    });
+  }
+};
+
+// filters
+export const productFiltersController = async (req, res) => {
+  try {
+    const { checked, radio } = req.body;
+    let args = {};
+    if (checked.length > 0) args.category = checked;
+    if (radio.length) args.price = { $gte: radio[0], $lte: radio[1] };
+    const products = await productModel.find(args);
+    res.status(200).send({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "Error WHile Filtering Products",
+      error,
+    });
+  }
+};
+
+export const productCountController = async (req, res) => {
+  try {
+    const total = await productModel.find({}).estimatedDocumentCount();
+    res.status(200).send({
+      success: true,
+      total,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "Error in counting Products",
+      error,
+    });
+  }
+};
+
+// product list base on page
+export const productListController = async (req, res) => {
+  try {
+    const perPage = 6;
+    const page = req.params.page ? req.params.page : 1;
+    const products = await productModel
+      .find({})
+      .select("-photo")
+      .skip((page - 1) * perPage)
+      .limit(perPage)
+      .sort({ createdAt: -1 });
+    res.status(200).send({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "error in per page ctrl",
+      error,
+    });
+  }
+};
+
+export const searchProductController = async (req, res) => {
+  try {
+    const { keyword } = req.params;
+    const results = await productModel({
+      $or: [
+        { name: { $regex: keyword, $options: "i" } },
+        { description: { $regex: keyword, $options: "i" } },
+      ],
+    }).select("-photo");
+    res.json(results);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "error in search control",
+      error,
+    });
+  }
+};
+export const relatedProductController = async (req, res) => {
+  try {
+    const { pid, cid } = req.params;
+    const products = await productModel
+      .find({
+        category: cid,
+        _id: { $ne: pid },
+      })
+      .select("-photo".limit(3).populate("category"));
+    res.status(200).send({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "error in getting related product",
+      error,
+    });
+  }
+};
+export const productCategoryController = async (req, res) => {
+  try {
+    const category = await categoryModel.findOne({
+      slug: req.params.slug,
+    });
+    const products = await productModel.find({ category }).populate("category");
+    res.status(200).send({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "error in getting category wise product",
+      error,
     });
   }
 };

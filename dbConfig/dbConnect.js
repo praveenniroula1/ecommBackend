caaabf99848c746cac83208cd450ebb9b36e3e4b
praveenniroula1 <101380394+praveenniroula1@users.noreply.github.com>
2024-01-16 01:05:23 +1030

    updated db config

--- dbConfig/dbConnect.js
+++ dbConfig/dbConnect.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 export const dbConnect = () => {
-  const conStr = process.env.MONGO_CLIENT;
-  const connecting = mongoose.connect(conStr);
+  const connectionProtocol = process.env.MONGO_CLIENT;
+  const conStr = mongoose.connect(connectionProtocol);
   if (conStr) {
     connecting && console.log("COnnected DAtabase");
   }
